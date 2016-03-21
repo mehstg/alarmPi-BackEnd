69554248151f46f5865571f0290f61e2852c67e5
root <root@localhost.localdomain>
2016-03-21 09:26:39 +0000

    updated test dictionary

--- flaskServer.py
+++ flaskServer.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-state = {u'State': u'Armed', u'Log': [{u'Date': u'20/01/2015', u'StateChange': u'Disarmed', u'Time': u'01:50'}, {u'Date': u'20/01/2015', u'StateChange': u'Armed', u'Time': u'07:50'}], u'Title': u'WansbeckAlarm'}
+state = {u'IsArmed': False, u'Log': [{u'Date': u'20/01/2015', u'StateChange': u'Disarmed', u'Time': u'01:50'}, {u'Date': u'20/01/2015', u'StateChange': u'Armed', u'Time': u'07:50'}], u'Title': u'WansbeckAlarm'}
 
 @app.route('/api/v1.0/getState', methods=['GET'])
 def get_tasks():
