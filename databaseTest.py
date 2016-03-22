d98828bd32ad4c46cc9ca702cac7d15c1aeb02a0
Paul Braham <paul@localhost.localdomain>
2016-03-22 10:29:34 +0000

    dev building output dictionary

--- databaseTest.py
+++ databaseTest.py
@@ -5,4 +5,16 @@ db = dbConnector('alarmPi.db')
 
 db.setRecord('reset')
 
-print db.getAllRecords()
+records = db.getAllRecords()
+
+print 'Printing whole records array:'
+print records
+
+output = []
+for record in records:
+    print 'Generating output dictionary'
+    output.append({u'epoch': record[1], u'StateChange': record[2]})
+
+print 'print output dictionary'
+
+print output
--- flaskServer.py
+++ flaskServer.py
@@ -1,12 +1,20 @@
 #!env/bin/python
 from flask import Flask, jsonify
+from dbConnector import dbConnector
 
 app = Flask(__name__)
+db = dbConnector('alarmPi.db')
+
+records = db.getAllRecords()
+
+
+
+
 
 state = {u'IsArmed': False, u'Log': [{u'Date': u'20/01/2015', u'StateChange': u'Disarmed', u'Time': u'01:50'}, {u'Date': u'20/01/2015', u'StateChange': u'Armed', u'Time': u'07:50'}], u'Title': u'WansbeckAlarm'}
 
 @app.route('/api/v1.0/getState', methods=['GET'])
-def get_tasks():
+def get_state():
     return jsonify(state)
 
 if __name__ == '__main__':
