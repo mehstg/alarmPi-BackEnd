fd9691d7407568c4625634d3d4316a1b6a75bb6c
root <root@localhost.localdomain>
2016-03-22 19:02:15 +0000

    updated test

--- databaseTest.py
+++ databaseTest.py
@@ -7,27 +7,21 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 db = dbConnector('alarmPi.db') 
 
-#db.setEvent('reset')
+db.setEvent('disarm')
 
 records = db.getAllEvents()
 
-print 'Printing whole records array:'
-print records
 
 output = []
 for record in records:
-    print 'Generating output dictionary'
     dt = time.strftime("%d-%m-%Y", time.gmtime(record[1]))
     tm = time.strftime("%H:%M:%S", time.gmtime(record[1]))
     output.append({u'Date': dt,  u'Time': tm, u'StateChange': record[2]})
 
-print 'print output dictionary'
-
-print output
 
 
 
-db.setState('arm')
 
-print 'Outputting state'
+db.setState('disarm')
+
 print db.getState()
